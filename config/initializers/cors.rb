20f40f93f23eb809dd638388414ba7d48d4fc51c
kurcevroman11 <kurcevroman11@gmail.com>
2025-08-24 22:31:37 +0300

    добавлены хост прода

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # Укажи фронтенд адрес (React/Vite)
     origins '*' # или '*' для разработки
 
     resource '*',
