a758db393601d3dc5572288dbb7789293a879f84
kurcevroman11 <kurcevroman11@gmail.com>
2025-08-25 22:53:23 +0300

    добавлен yande cloud

--- app/blueprints/pdf_document_blueprint.rb
+++ app/blueprints/pdf_document_blueprint.rb
@@ -3,10 +3,8 @@ class PdfDocumentBlueprint < Blueprinter::Base
   fields :watermark, :created_at
 
   view :with_url do
-    field :url do |obj, options|
-      # options[:host] можно передавать при рендере
-      host = options[:host] || ENV["APP_HOST"]
-      Rails.application.routes.url_helpers.rails_blob_url(obj.file, host: host, only_path: false)
+    field :url do |obj|
+      obj.file.url
     end
   end
 end
